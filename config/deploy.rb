12b653e69e60e2e99c579624abce73266ee477bf
joshlin <joshlin@uw.edu>
2016-09-01 17:22:56 -0700

    update deploy script and add configuration for subdiretory

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,19 +31,23 @@ set :repo_url,  "git@github.com:uwexpd/expo2.git"
 set :scm, "git"
 set :branch, 'master'
 set :deploy_user, "joshlin"
+server 'expd.uaa.washington.edu', user: 'joshlin', roles: %w{web app db}, primary: true
+set :rvm_ruby_version, "2.3.1" # set up which rvm ruby to use in server
 
 # Tell cap your own private keys for git and use agent forwarding with this command.
 set :ssh_options, {
   forward_agent: true
 }
 
-set :pty, true # Must be set for the password prompt from git to work
-
-set :rvm_ruby_string, "2.3.1" # set up which rvm ruby to use in server
-
-  server 'expd.uaa.washington.edu', user: 'joshlin', roles: %w{web app db}, primary: true
+#set :pty, true # Must be set for the password prompt from git to work
 
 set :keep_releases, 10
+
+# files we want symlinking to specific entries in shared.
+set :linked_files, %w{config/database.yml}
+
+# dirs we want symlinking to shared
+set :linked_dirs, %w{config/certs}
 
 
 
