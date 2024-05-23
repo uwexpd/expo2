a5c36aa8f422826f7b9ee9b64313a2359487c49d
joshlin <joshlin@uw.edu>
2024-05-23 16:46:09 -0700

    add admin update application function and fix extra dept bug

--- app/admin/department_extra.rb
+++ app/admin/department_extra.rb
@@ -10,19 +10,19 @@ ActiveAdmin.register DepartmentExtra do
   index do         
      column (:id) {|dept_extra| link_to dept_extra.id, admin_department_extra_path(dept_extra)}
      column ("SDB Department Name"){|dept_extra| dept_extra.department.dept_full_nm.strip.titleize.gsub("Of", "of") if dept_extra.dept_code}
-     column :fixed_name, sortable: :name do |resource| 
-       editable_text_column resource, "department_extras", :fixed_name, true
+     column :fixed_name, sortable: :name do |dept_extra| 
+       editable_text_column dept_extra, "department_extra", :fixed_name, true
      end     
      column ("College Name"){|dept_extra| dept_extra.college.full_name if dept_extra.dept_code}
      # column ("College Campus"){|dept_extra| dept_extra.college.campus_name}
-     column :chair_name do |resource| 
-       editable_text_column resource, "department_extras", :chair_name, false
+     column :chair_name do |dept_extra| 
+       editable_text_column dept_extra, "department_extra", :chair_name, false
      end
      # column :chair_email do |resource| 
-     #   editable_text_column resource, "department_extras", :chair_email, false
+     #   editable_text_column resource, "department_extra", :chair_email, false
      # end
      # column :chair_title do |resource| 
-     #   editable_text_column resource, "department_extras", :chair_title, false
+     #   editable_text_column resource, "department_extra", :chair_title, false
      # end
      actions
   end   
