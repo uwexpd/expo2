c2e92b16d5b95877e7c51a4ffc32a35c6a59bd1d
joshlin <joshlin@uw.edu>
2024-08-12 15:09:14 -0700

    fix remove vicarious login path issue and improve community engagement landing page performace

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,10 +6,12 @@ class WelcomeController < ApplicationController
     redirect_to admin_path if @current_user.admin?
     
     @person = @current_user.person
+    @person_apps = @person.application_for_offerings
     @student = @person if @person.is_a?(Student)
-    @service_learning_courses = @student ? @student.enrolled_service_learning_courses(Quarter.where("year > ?", 2007).to_a) : @person.service_learning_courses
-    
-    @units = Unit.with_description.for_welcome_screen.order("RAND()").limit(3)
+    @service_learning_courses = @student ? @student.enrolled_service_learning_courses(Quarter.current_quarter, nil) : @person.service_learning_courses
+    #[TODO] query previous quarters: @student.enrolled_service_learning_courses(Quarter.where("year > ?", 2007).to_a, nil) when request
+
+    # @units = Unit.with_description.for_welcome_screen.limit(3)
     
   end
 
