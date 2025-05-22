7e28bf9b0192288d4832fa644fb812dfec1cb702
joshlin <joshlin@uw.edu>
2025-05-21 17:04:01 -0700

    update view caches to be persistent and only refreshed when applicaiton is saved so that we can better support the symposium event day's heavy traffic.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  # For Debug need
+  # config.cache_store = :file_store, "#{Rails.root}/tmp/cache"
+  # config.action_controller.enable_fragment_cache_logging = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
